757d6105a15232570e0c0fa57aa961f2879525b3
Miguel Ángel Mendoza <miguel.mendoza@totemti.com>
2016-05-18 17:18:30 -0500

    Add first code

--- examples/config.php
+++ examples/config.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-  'api_key' => 'ca2a7238-6cef-465a-89aa-411ba5803de6' // ElasticEmail APY KEY
-  ,'email_account' => 'miguel.mendoza@totemti.com' // ElasticEmail EMAIL ACCOUNT
+  'api_key' => 'YOUR-API-KEY' // ElasticEmail APY KEY
+  ,'email_account' => 'YOUR-EMAIL-ACCOUNT' // ElasticEmail EMAIL ACCOUNT
 );
